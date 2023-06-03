6ae6686a4b49a7e056efb6977a6ca28ca9532809
Sebastián Burgos <snburgos@uc.cl>
2023-06-03 13:41:36 -0400

    Doing the home view

--- meteo-09/src/App.vue
+++ meteo-09/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import HomeView from './views/HomeView.vue';
 </script>
-<script lang="ts">
+<!-- <script lang="ts">
 var info = {}
 export default {
   mounted() {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=-33.45694&lon=-70.64827&appid=ddec887a4abcfa9dca8520346d2b065c')
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=-33.45694&lon=-70.64827&appid=ddec887a4abcfa9dca8520346d2b065c&units=metric&lang=es')
       .then(response => response.json())
       .then(data => {
         // Aquí puedes acceder a los datos de respuesta y utilizarlos en tu componente
@@ -18,7 +18,7 @@ export default {
       });
   }
 }
-</script>
+</script> -->
 
 <template>
   <div class="section">
--- meteo-09/src/views/HomeView.vue
+++ meteo-09/src/views/HomeView.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const periods = ['Hoy', 'Esta semana', 'Próximos 16 días']
+
+const selectedPeriod = ref(periods[0])
+
+function selectPeriod (period: string) {
+  selectedPeriod.value = period
+}
 </script>
 
 <template>
   <nav class="is-primary panel">
     <span class="panel-tabs">
-      <a>Today</a>
+      <!-- : -> v-bind: and @ -> v-on: -->
+      <a v-for="period of periods" :key="period" :class="{ 'is-active': period === selectedPeriod }" @click="selectPeriod(period)">
+        {{ period }}
+      </a>
     </span>
+    {{ selectedPeriod }}
   </nav>
 </template>
