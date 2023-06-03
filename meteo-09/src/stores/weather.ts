e49dcc7c73ef1586b9a9f3a92973bc412c5bd4d0
Sebastián Burgos <snburgos@uc.cl>
2023-06-03 19:43:17 -0400

    Added filtered weathers for today and tomorrow

--- meteo-09/src/stores/weather.ts
+++ meteo-09/src/stores/weather.ts
@@ -1,9 +1,12 @@
 import { defineStore } from 'pinia'
+import { DateTime } from 'luxon'
 import { Weather } from '../scripts/weather'
+import { Period } from '../scripts/constants'
 
 interface WeatherState {
   weather: Weather
   weathers: Weather[]
+  selectedPeriod: Period
 }
 
 export const useWeather = defineStore('weathers', {
@@ -17,15 +20,34 @@ export const useWeather = defineStore('weathers', {
       pressure: 0,
       humidity: 0
     },
-    weathers: []
+    weathers: [],
+    selectedPeriod: 'Ahora'
   }),
 
   actions: {
+    setSelectedPeriod(period: Period) {
+      this.selectedPeriod = period
+    },
     updateWeather(weather: Weather) {
       this.weather = weather
     },
     updateWeathers(weathers: Weather[]) {
       this.weathers = weathers
+    }
+  },
+
+  getters: {
+    filteredWeathers: (state): Weather[] => {
+      switch (state.selectedPeriod) {
+        case 'Ahora':
+          return [state.weather]
+        case 'Hoy':
+          return state.weathers.filter(weather => DateTime.fromFormat(weather.dt, 'ff').hasSame(DateTime.now().toLocal(), 'day'))
+        case 'Mañana':
+          return state.weathers.filter(weather => DateTime.fromFormat(weather.dt, 'ff').hasSame(DateTime.now().toLocal().plus({ days: 1 }), 'day'))
+        case 'Próximos 5 días':
+          return state.weathers
+      }
     }
   }
 })
--- meteo-09/src/views/HomeView.vue
+++ meteo-09/src/views/HomeView.vue
@@ -1,36 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useWeather } from '../stores/weather'
 import WeatherItem from '../components/WeatherItem.vue';
+import { periods } from '../scripts/constants';
 
 const weatherStore = useWeather()
-
-const periods = ['Ahora', 'Próximos 5 días'] as const
-
-type Period = typeof periods[number]
-
-const selectedPeriod = ref<Period>(periods[0])
-
-function selectPeriod (period: Period) {
-  selectedPeriod.value = period
-}
-
-// const weathers = computed<Weather[]>(() => [
-//   today,
-//   thisWeek,
-//   sixteenDays
-// ].filter(weather => weather.period === selectedPeriod.value))
 </script>
 
 <template>
   <nav class="is-primary panel">
     <span class="panel-tabs">
       <!-- : -> v-bind: and @ -> v-on: -->
-      <a v-for="period of periods" :key="period" :class="{ 'is-active': period === selectedPeriod }" @click="selectPeriod(period)">
+      <a v-for="period of periods" :key="period" :class="{ 'is-active': period === weatherStore.selectedPeriod }" @click="weatherStore.setSelectedPeriod(period)">
         {{ period }}
       </a>
     </span>
-    <WeatherItem v-if="selectedPeriod === 'Ahora'" :weather="weatherStore.weather" />
-    <WeatherItem v-else v-for="weather of weatherStore.weathers" :key="weather.dt" :weather="weather" />
+    <WeatherItem v-for="weather of weatherStore.filteredWeathers" :key="weather.dt" :weather="weather" />
   </nav>
 </template>
